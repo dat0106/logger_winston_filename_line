cd42e1b31a52b6854e89d78311b77d96342184fc
Dat-outsource <datkts0106@gmail.com>
2022-05-14 09:49:55 +0700

    init code

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const logger = require('./logger');
+const app = express();
+const port = 3000;
+
+app.get('/', function(req, res) {
+    res.send("Hello World");
+})
+
+app.listen(port, function(error) {
+    if (error) {
+        logger.error("Something went wrong");
+    }
+    logger.info('hello', { value: 'world' });
+    logger.info('hello', { value: 'world' }, { value: 'world' });
+    logger.info(undefined);
+    logger.info(null);
+    logger.info({});
+    logger.info("dat");
+    logger.info(1334);
+    console.log("server is running port:  " + port);
+})
